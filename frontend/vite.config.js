d257ca13403ac00048af5b9d05c86a6884df372a
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-03-24 00:49:12 +0530

    final commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'http://hitesh2.ap-south-1.elasticbeanstalk.com',
         changeOrigin: true,
         rewrite: (path) => path.replace('/^\/api/', ''),
-        secure: false
+        secure: true
       }
     }
   }
