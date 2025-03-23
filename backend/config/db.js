222681d377a704149bd78aef9469b77756ddd9b2
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-03-23 22:47:52 +0530

    final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose"
  export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://pawarhitesh321:Hitesh123@cluster0.ot3o8an.mongodb.net/Food_Delivery').then(()=>console.log("DB Connected"))
+    await mongoose.connect(process.env.DBLink).then(()=>console.log("DB Connected"))
 }
