e05605a6ba52194368ea712940ecde1453fbe3f3
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-03-24 09:19:48 +0530

    final commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -46,8 +46,8 @@ const placeOrder = async (req,res) => {
         const session = await stripe.checkout.sessions.create({
             line_items:line_items,
             mode:'payment',
-            success_url:`${backend_url}/api/order/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url:`${backend_url}/api/order/verify?success=false&orderId=${newOrder._id}`,
+            success_url:`${backend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${backend_url}/verify?success=false&orderId=${newOrder._id}`,
         })
         res.json({success:true,session_url:session.url,id:newOrder._id})
 
